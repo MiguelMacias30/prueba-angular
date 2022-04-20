43d852a5f1711784b2e675fe185f7c96dedb92b1
miguel <miguelfernandomacias@gmail.com>
2022-04-20 12:37:39 -0400

    right component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftComponent } from './common/left.component';
+import { RightComponent } from './common/right.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftComponent,
+    RightComponent
   ],
   imports: [
     BrowserModule,
